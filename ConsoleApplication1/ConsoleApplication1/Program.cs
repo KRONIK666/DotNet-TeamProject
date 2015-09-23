6bdd4808b9b0b2ce35bee3acc6c428a022ab0b79
meg4ei <meg.glupost@gmail.com>
2015-09-23 21:43:18 +0300

    k

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Мачкааааййй Лавинаааа!");
             Console.WriteLine("Победа!");
+
+            Console.WriteLine("gfgfgf");
         }
     }
 }
