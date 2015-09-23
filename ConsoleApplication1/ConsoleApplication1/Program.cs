5417be0779903649f69faf3a4541b3be72eecb2f
ppenkov <penkov_law@abv.bg>
2015-09-23 21:26:05 +0300

    Commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Мачкааааййй!");
+            Console.WriteLine("No 1");
         }
     }
 }
