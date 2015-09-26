254d284020536d4b44d2a54aeef6bcfeb0ae2099
ppenkov <penkov_law@abv.bg>
2015-09-26 13:17:17 +0300

    Project

--- MonthlyBudgetProject/MonthlyBudgetProject/Home Page.aspx.cs
+++ MonthlyBudgetProject/MonthlyBudgetProject/Home Page.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,32 @@ namespace MonthlyBudgetProject
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void chooseExpense_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if ((chooseExpense.SelectedIndex !=0) && (calendar.SelectedDate.Year > 1)) 
+            {
+                submitBtn.Enabled = true;
+            }
+        }
+
+        protected void submitBtn_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter(Server.MapPath("FilePath"), true))
+            {
+                writer.WriteLine(Date.Text + " " + calendar.SelectedDate);
+                writer.WriteLine(Expense.Text + " " + chooseExpense.SelectedIndex);
+                writer.WriteLine(Price.Text + " " + priceTxtBox.TextMode);
+            }
+        }
+
+        protected void calendar_SelectionChanged(object sender, EventArgs e)
+        {
+            if ((chooseExpense.SelectedIndex != 0) && (calendar.SelectedDate.Year > 1))
+            {
+                submitBtn.Enabled = true;
+            }
         }
     }
 }
