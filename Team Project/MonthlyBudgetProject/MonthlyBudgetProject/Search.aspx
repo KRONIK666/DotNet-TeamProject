cac6e99a8b99ca0e6852d3611799cc5c11eb30ad
KRONIK <kronik@abv.bg>
2016-10-19 19:12:45 +0300

    Team Project

---
+++ Team Project/MonthlyBudgetProject/MonthlyBudgetProject/Search.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="MonthlyBudgetProject.Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Expenses Statistics</title>
+    <style>
+        .button {
+            margin-left: 10px;
+        }
+
+        #searchWeek,#searchMonth,#searchYear {
+            float: right;
+            margin-left: 10px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form" runat="server">
+        <div>
+            <asp:LinkButton Text="Home Page" runat="server" CssClass="button" PostBackUrl="~/Home Page.aspx" />
+            <asp:LinkButton Text="Expenses Statistics" runat="server" CssClass="button" PostBackUrl="~/Search.aspx" />
+            <br />
+            <br />
+        </div>
+        <div style="float: left; margin-left: 10px;">
+            <asp:Label Text="Last Week: " runat="server" Font-Bold="True" />
+            <asp:Button Text="Search" runat="server" ID="searchWeek" OnClick="searchWeek_Click" />
+            <br />
+            <br />
+            <asp:Label Text="Last Month: " runat="server" Font-Bold="True" />
+            <asp:Button Text="Search" runat="server" ID="searchMonth" OnClick="searchMonth_Click" />
+            <br />
+            <br />
+            <asp:Label Text="Last Year: " runat="server" Font-Bold="True" />
+            <asp:Button Text="Search" runat="server" ID="searchYear" OnClick="searchYear_Click" />
+            <br />
+            <br />
+        </div>
+        <div style="float: left; padding-left: 50px;">
+            <asp:Repeater runat="server" ID="infoRepeater">
+                <ItemTemplate>
+                    <asp:Label Text='<%# Eval("Week") %>' runat="server" />
+                    <br />
+                    <asp:Label Text='<%# Eval("Month") %>' runat="server" />
+                    <br />
+                    <asp:Label Text='<%# Eval("Year") %>' runat="server" />
+                    <br />
+                    <br />
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Team Project/MonthlyBudgetProject/MonthlyBudgetProject/Search.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MonthlyBudgetProject
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        private double weeklySumCalculated = 0;
+        private double monthlySumCalculated = 0;
+        private double annualSumCalculated = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void searchWeek_Click(object sender, EventArgs e)
+        {
+            using (StreamReader reader = new StreamReader(ConfigurationManager.AppSettings["FilePath"], true))
+            {
+                string line = reader.ReadLine();
+
+                do
+                {
+                    string[] sum = line.Split('|');
+
+                    if (line == "25.10.2016 г.|Vacation|200")
+                    {
+                        weeklySumCalculated = (double.Parse(sum[2]) + double.Parse(sum[5]) + double.Parse(sum[11]) + double.Parse(sum[23]));
+
+                        using (StreamWriter writer = new StreamWriter(ConfigurationManager.AppSettings["FilePath"], true))
+                        {
+                            writer.WriteLine(weeklySumCalculated);
+                        }
+                    }
+                } while (line != null);
+            }
+        }
+
+        protected void searchMonth_Click(object sender, EventArgs e)
+        {
+            using (StreamReader reader = new StreamReader(ConfigurationManager.AppSettings["FilePath"], true))
+            {
+                string line = reader.ReadLine();
+
+                do
+                {
+                    string[] sum = line.Split('|');
+
+                    if (line == "25.10.2016 г.|Vacation|200")
+                    {
+                        monthlySumCalculated = (double.Parse(sum[2]) + double.Parse(sum[5]) + double.Parse(sum[8]) + double.Parse(sum[11]) + double.Parse(sum[14]) + double.Parse(sum[17]) + double.Parse(sum[20])
+                             + double.Parse(sum[23]) + double.Parse(sum[26]) + double.Parse(sum[29]));
+
+                        using (StreamWriter writer = new StreamWriter(ConfigurationManager.AppSettings["FilePath"], true))
+                        {
+                            writer.WriteLine(monthlySumCalculated);
+                        }
+                    }
+                } while (line != null);
+            }
+        }
+
+        protected void searchYear_Click(object sender, EventArgs e)
+        {
+            using (StreamReader reader = new StreamReader(ConfigurationManager.AppSettings["FilePath"], true))
+            {
+                string line = reader.ReadLine();
+
+                do
+                {
+                    string[] sum = line.Split('|');
+
+                    if (line == "25.10.2016 г.|Vacation|200")
+                    {
+                        annualSumCalculated = (double.Parse(sum[2]) + double.Parse(sum[5]) + double.Parse(sum[8]) + double.Parse(sum[11]) + double.Parse(sum[14]) + double.Parse(sum[17]) + double.Parse(sum[20])
+                             + double.Parse(sum[23]) + double.Parse(sum[26]) + double.Parse(sum[29]));
+
+                        using (StreamWriter writer = new StreamWriter(ConfigurationManager.AppSettings["FilePath"], true))
+                        {
+                            writer.WriteLine(annualSumCalculated);
+                        }
+                    }
+                } while (line != null);
+            }
+        }
+    }
+}
